77e21cef032772b9941d1036fd87561a349f0ab0
talktojoegee <talktojoegee@gmail.com>
2023-07-11 09:59:39 +0100

    slider description removed

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -18,49 +18,6 @@
                             <!-- Background -->
                             <img src="/assets/images/slider.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina data-kenburns="on" data-duration="12000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="100" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0">
 
-                            <!-- Caption-->
-                            <div class="tp-caption custom-caption-2 tp-shape tp-shapewrapper tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-1-layer-2"
-                                 data-x="['left','left','left','left']"
-                                 data-hoffset="['0','40','40','40']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0']"
-                                 data-width="['640','640', 640','420','320']"
-                                 data-height="auto"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-                                 data-transform_in="y:0;opacity:0;s:1000;e:Power2.easeOutExpo;s:400;e:Power2.easeOutExpo"
-                                 data-transform_out=""
-                                 data-mask_in="x:0px;y:[20%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1000"
-                                 data-responsive_offset="on">
-
-                                <!-- Caption Content -->
-                                <div class="R_title margin-bottom-15"
-                                     id="slide-2-layer-1"
-                                     data-x="['left','center','center','center']"
-                                     data-hoffset="['0','0','40','0']"
-                                     data-y="['middle','middle','middle','middle']"
-                                     data-voffset="['-40','-40','-20','-80']"
-                                     data-fontsize="['42','36', '32','36','22']"
-                                     data-lineheight="['70','60','60','45','35']"
-                                     data-width="['640','640', 640','420','320']"
-                                     data-height="none" data-whitespace="normal"
-                                     data-transform_idle="o:1;"
-                                     data-transform_in="y:-50px;sX:2;sY:2;opacity:0;s:1000;e:Power4.easeOut;"
-                                     data-transform_out="opacity:0;s:300;"
-                                     data-start="600"
-                                     data-splitin="none"
-                                     data-splitout="none"
-                                     data-basealign="slide"
-                                     data-responsive_offset="off"
-                                     data-responsive="off"
-                                     style="z-index: 6; color: #fff; letter-spacing: 0px; font-weight: 600; ">Discover City Gems</div>
-
-                                <div class="caption-text">Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</div>
-                                <a href="#" class="button medium">Get Started</a>
-                            </div>
-
                         </li>
 
                         <!-- Slide  -->
@@ -69,47 +26,6 @@
                             <!-- Background -->
                             <img src="/assets/images/slider.jpg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina data-kenburns="on" data-duration="12000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="112" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0">
 
-                            <!-- Caption-->
-                            <div class="tp-caption centered custom-caption-2 tp-shape tp-shapewrapper tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-2-layer-2"
-                                 data-x="['center','center','center','center']" data-hoffset="['0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0']"
-                                 data-width="['640','640', 640','420','320']"
-                                 data-height="auto"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-                                 data-transform_in="y:0;opacity:0;s:1000;e:Power2.easeOutExpo;s:400;e:Power2.easeOutExpo"
-                                 data-transform_out=""
-                                 data-mask_in="x:0px;y:[20%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1000"
-                                 data-responsive_offset="on">
-
-                                <!-- Caption Content -->
-                                <div class="R_title margin-bottom-15"
-                                     id="slide-2-layer-3"
-                                     data-x="['center','center','center','center']"
-                                     data-hoffset="['0','0','0','0']"
-                                     data-y="['middle','middle','middle','middle']"
-                                     data-voffset="['-40','-40','-20','-80']"
-                                     data-fontsize="['42','36', '32','36','22']"
-                                     data-lineheight="['70','60','60','45','35']"
-                                     data-width="['640','640', 640','420','320']"
-                                     data-height="none" data-whitespace="normal"
-                                     data-transform_idle="o:1;"
-                                     data-transform_in="y:-50px;sX:2;sY:2;opacity:0;s:1000;e:Power4.easeOut;"
-                                     data-transform_out="opacity:0;s:300;"
-                                     data-start="600"
-                                     data-splitin="none"
-                                     data-splitout="none"
-                                     data-basealign="slide"
-                                     data-responsive_offset="off"
-                                     data-responsive="off"
-                                     style="z-index: 6; color: #fff; letter-spacing: 0px; font-weight: 600; ">Streamline Your Business</div>
-
-                                <div class="caption-text">Proactively envisioned multimedia based on expertise cross-media growth strategies. Pontificate installed base portals after maintainable products.</div>
-                                <a href="index-5.html#" class="button medium">Read More</a>
-                            </div>
 
                         </li>
 
